30dc631584706332c3266e9c8d45865316261100
ShahmirFarooqui <shahmirfarooqui91@gmail.com>
2020-10-31 20:47:36 -0400

    created header and table

---
+++ src/components/table/Table.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// a function that returns the table body on the page
+  
+import React from "react";
+
+const Table = (props) => {
+  return (
+    <tbody>
+          <tr>
+            <td>
+              <img src={props.image} alt="" />
+            </td>
+            <td>{props.first}</td>
+            <td>{props.last}</td>
+            <td>{props.email}</td>
+            <td>{props.age}</td>
+          </tr>
+   </tbody>
+  );
+};
+
+export default Table;
